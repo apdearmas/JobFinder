f2eaa74f61b08ce9dd1d08ba6576c783425bfddb
FRANCISCO GONGORA <fgongora67@gmail.com>
2015-01-20 18:19:35 -0500

    Added business logic to send email.

---
+++ BDL/EmailService.cs
@@ -0,0 +1,59 @@
+﻿using BDL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+
+namespace BDL
+{
+    public class EmailService : IEmailService
+    {
+        public SmtpClient smtpClient;
+        public MailMessage mail;
+
+        const int       SmtpPort =      587;
+        const string    Host =         "plus.smtp.mail.yahoo.com";
+        const string    MailFrom =     "fgongora67@yahoo.com";
+        const string    Password =     "Gabriela01";
+       
+        
+
+        public EmailService()
+        {
+            ConfigureSmtpClient();
+            ConfigureMail();
+
+        }
+                
+
+        private void ConfigureMail()
+        {
+            mail = new MailMessage();
+            mail.From = new MailAddress(EmailService.MailFrom);
+
+        }
+
+        private void ConfigureSmtpClient()
+        {
+            smtpClient = new SmtpClient(Host);
+            smtpClient.Port = SmtpPort;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(EmailService.MailFrom, EmailService.Password);
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+        }
+
+        public void Send(string targetEmail, string subject, string message)
+        {
+            mail.To.Add(targetEmail);
+            mail.Subject = subject;
+            mail.Body = message;
+
+            smtpClient.Send(mail);
+           
+        }
+        
+    }
+}
